21c4fe002218926f83a56efec11e3102e57717a7
Wumaomaomao <100843881+Wumaomaomao@users.noreply.github.com>
2022-06-30 23:03:00 +0800

    Add files via upload

---
+++ Oslab/lab3-201220102武雅琛/lab3/kernel/include/device/serial.h
@@ -0,0 +1,11 @@
+#ifndef __SERIAL_H__
+#define __SERIAL_H__
+
+void initSerial(void);
+void putChar(char);
+void putString(const char *str);
+void putInt(int i);
+
+#define SERIAL_PORT  0x3F8
+
+#endif
